edf63e284337e2496043ca2848160c7cd46cc586
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-15 23:30:30 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	import About from "./components/About.svelte";
+	export let name;
+	export let lastName;
+	let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
+</script>
+
+<style>
+
+	:global(body) { /* :global para generar los estilos no solo al doc local sino para toda la aplicación*/
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+
+	:global(:root) { /* Para crear mis variables de estilos */
+		--theme-color: purple;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<main>
+	<h1 class="title-text">Hello {name} {lastName}!</h1>
+	<About />
+	<img src={svelteLogo} alt="Svelte" />
+</main>
