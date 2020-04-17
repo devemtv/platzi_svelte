23b2dcf05eef102830f7d409b4ff624836cc8bd5
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-16 22:42:53 -0400

    Add conditionals 'if' 'else'

--- src/App.svelte
+++ src/App.svelte
@@ -24,6 +24,15 @@
 		--theme-color: purple;
 	}
 
+	:global(body.dark-mode) {
+		background-color: #1d3040;
+		color: #bfc2c7;
+	}
+
+	:global(body.dark-mode p) {
+		color: #bfc2c7;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,15 @@
 <script>
   let someText = `Frontend Developer at Platzi`;
   let count = 0;
+  let styles = { darkmode: false };
 
   function countClick() {
     count += 1;
+  }
+
+  function toggle() {
+    styles.darkmode = !styles.darkmode;
+    window.document.body.classList.toggle("dark-mode");
   }
 </script>
 
@@ -14,8 +20,15 @@
 </style>
 
 <div class="About">
-  <p>
-    {someText}
-  </p>
+
+  {#if !styles.darkmode}
+    <p>{someText}</p>
+  {:else}
+    <p>
+      <span>Hello DarkMode</span>
+    </p>
+  {/if}
+
   <button on:click={countClick}>Click {count > 0 ? count : ''}</button>
+  <button on:click={toggle}>Darkmode</button>
 </div>
