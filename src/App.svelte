6ef30a0f059d72df0373481fd13793d200b04934
Miguel Torrealba <mtorrealba@techk.cl>
2020-04-16 22:56:49 -0400

    Each and add component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 	import About from "./components/About.svelte";
 	import Text from "./components/Text.svelte";
 	import Person from "./components/Person.svelte";
+	import Skills from "./components/Skills.svelte";
+
 	export let name;
 	export let lastName;
 	let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
@@ -64,5 +66,6 @@
 	<Text anotherText="Hola!" />
 	<Text />
 	<Person {...data} />
+	<Skills />
 	<img src={svelteLogo} alt="Svelte" />
 </main>
